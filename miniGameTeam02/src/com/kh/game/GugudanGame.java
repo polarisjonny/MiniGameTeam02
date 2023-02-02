60a08128116a64d022f506a33d20810d671b1b0e
DESKTOP-DK4M6F2\user2 <joeun9705@gmail.com>
2023-02-02 19:57:06 +0900

    feat: add features of gugudan game

---
+++ miniGameTeam02/src/com/kh/game/GugudanGame.java
@@ -0,0 +1,37 @@
+package com.kh.game;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GugudanGame {
+	int randomNumber = 0;
+	int y = 0;
+	int userNumber = 0;
+	
+	int answer = 0;
+	
+	Scanner scanner = new Scanner(System.in);
+	
+	Random random = new Random();
+	
+	GugudanGame() {
+		System.out.println("--------------------------");
+		System.out.println("|    구구단 게임을 시작합니다   |");
+		System.out.println("--------------------------");
+		
+		startGugudanGame();
+	}
+	
+	public void startGugudanGame() {
+		randomNumber = random.nextInt(9)+1;
+		y = random.nextInt(9) + 1;
+		answer = randomNumber * y;
+		System.out.println(randomNumber+"X"+y+"= ?");
+		userNumber = scanner.nextInt();
+		
+		if(userNumber == answer) {
+			System.out.println("정답입니다!");
+		} else {
+			System.out.println("오답입니다 ㅠㅠ");
+		}
+	}
+}
